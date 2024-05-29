73eb591ea1e2eafdaff0369d69c75ee7b87bf224
yfchenkeepgoing <chenyifanxjtu.phy@gmail.com>
2024-05-29 15:30:29 +0100

    Implement basic front-end and back-end interaction

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -13,22 +13,11 @@ import java.util.Map;
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getBotInfo() {
-        List<Map<String, String>> list = new LinkedList<>();
+    public Map<String, String> getBotInfo() {
         // bot1
         Map<String, String> bot1 = new HashMap<>();
-        bot1.put("name", "tiger");
+        bot1.put("name", "apple");
         bot1.put("rating", "1500");
-
-        // bot2
-        Map<String, String> bot2 = new HashMap<>();
-        bot2.put("name", "apple");
-        bot2.put("rating", "1800");
-
-        // add bot to list
-        list.add(bot1);
-        list.add(bot2);
-
-        return list;
+        return bot1;
     }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <div>Bot昵称: {{ bot_name }}</div>
+    <div>Bot战力: {{ bot_rating }}</div>
   </div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue'; // 定义两个变量需要引入的包
+
+export default {
+  name: "App", // 名字随便起
+
+  // setup是整个函数的入口
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    // 用ajax取出后端的值，放入bot_name和bot_rating中，然后再放入页面里
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",
+      type: "get", // 两种请求：get: 获取数据; post: 创建数据
+      success: resp => {
+        // console.log(resp); // 调试用
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    });
+
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("./assets/background.png");
+  background-size: cover; 
 }
 </style>
+
