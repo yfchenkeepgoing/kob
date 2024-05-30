abaf4f230e65f3ee1880a26f38924e66597518ea
yfchenkeepgoing <chenyifanxjtu.phy@gmail.com>
2024-05-30 14:26:12 +0100

    Implement a navigation bar for the web front-end

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,38 +1,17 @@
 <template>
-  <div>
-    <div>Bot昵称: {{ bot_name }}</div>
-    <div>Bot战力: {{ bot_rating }}</div>
-  </div>
-  <router-view/>
+  <NavBar />
+  <router-view></router-view>
 </template>
 
 <script>
-import $ from 'jquery';
-import { ref } from 'vue'; // 定义两个变量需要引入的包
+import NavBar from './components/NavBar.vue'
+// 导入Bootstrap的css和js
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 export default {
-  name: "App", // 名字随便起
-
-  // setup是整个函数的入口
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    // 用ajax取出后端的值，放入bot_name和bot_rating中，然后再放入页面里
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getbotinfo/",
-      type: "get", // 两种请求：get: 获取数据; post: 创建数据
-      success: resp => {
-        // console.log(resp); // 调试用
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-
-    return {
-      bot_name,
-      bot_rating
-    }
+  components: {
+    NavBar
   }
 }
 </script>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// 引入5个页面
+import PkindexView from './../views/pk/PkindexView.vue'
+import RecordIndexView from './../views/record/RecordIndexView.vue'
+import RanklistIndexView from './../views/ranklist/RanklistIndexView.vue'
+import UserBotIndexView from './../views/user/bots/UserBotIndexView.vue'
+import NotFound from './../views/error/NotFound.vue'
+
 
 const routes = [
+  // 根路径重定向到对战页面
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/",
+  },
+  {
+    path: "/pk/", // 域名后的相对路径
+    name: "pk_index",
+    component: PkindexView,
+  },
+  {
+    path: "/record/", // 域名后的相对路径
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/", // 域名后的相对路径
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/user/bot/", // 域名后的相对路径
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/404/", // 域名后的相对路径
+    name: "404",
+    component: NotFound,
+  },
+  // 从上往下匹配，输入其他路径则跳转到404页面
+  // :catchAll(.*)会匹配所有字符
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/",
+  }
 ]
 
 const router = createRouter({
